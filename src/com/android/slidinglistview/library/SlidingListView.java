3cc55be384a3aa4265354522ebffe63d2b773fe9
Mehul Parmar <mehul.p@peopleinteractive.in>
2015-06-12 15:17:47 +0530

    Initial Commit

---
+++ src/com/android/slidinglistview/library/SlidingListView.java
@@ -0,0 +1,43 @@
+package com.android.slidinglistview.library;
+
+import com.android.slidinglistviewlibrarynsample.R;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.view.ViewConfigurationCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.ListView;
+
+public class SlidingListView extends ListView {
+
+    public final static int SLIDE_MODE_NONE = 0;
+
+    public final static int SLIDE_MODE_BOTH = 1;
+
+    public final static int SLIDE_MODE_RIGHT = 2;
+
+    public final static int SLIDE_MODE_LEFT = 3;
+	
+	int slideFrontView, slideBackView;
+	boolean openSlidingWhenLongPressed, openSlideRight, openSlideLeft, multipleViewsOpen, slideMode, closeAllItemsOnListScroll;
+	int SLIDEMODE_LEFT, SLIDEMODE_RIGHT, SLIDEMODE_NONE, SLIDEMODE_BOTH;
+	
+	public SlidingListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs);
+	}
+	
+	public SlidingListView(Context context, int slideFrontView, int slideBackView) {
+        super(context);
+        this.slideFrontView = slideFrontView;
+        this.slideBackView = slideBackView;
+        init(null);
+    }
+	
+	private void init(AttributeSet attrs) {
+        openSlidingWhenLongPressed = true;
+        closeAllItemsOnListScroll = true;
+	}
+}
